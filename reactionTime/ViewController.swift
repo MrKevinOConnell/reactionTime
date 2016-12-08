efad4c94aed602fcf6d300c804c42ce49df6f5b0
Kevin O'Connell <koconnell1@kevins-macbook-air-6.local>
2016-12-08 10:18:53 -0600

    Initial Commit

---
+++ reactionTime/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  reactionTime
+//
+//  Created by Kevin O'Connell on 12/1/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+import UIKit
+var time = timer()
+class ViewController: UIViewController {
+    
+
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+ 
+    @IBAction func startButton(_ sender: UIButton) {
+    
+    view.backgroundColor = UIColor.red
+        //changes background color to red
+    func randomTimer() -> Int {
+        let randomIndex = Int(arc4random_uniform(UInt32(10)))
+        print(randomIndex)
+       
+        let counter = 10
+        var myTimer = counter + randomIndex
+        
+        var randomTime = myTimer + randomIndex
+        return randomTime }
+   
+    }
+    @IBAction func endButton(_ sender: UIButton) {
+    
+               
+    
+            
+        }
+         
+            
+            
+        
+    
+   
+    
+    
+        
+        
+               }
+        
+            
+    
---
+++ reactionTime/timer.swift
@@ -0,0 +1,25 @@
+//
+//  timer.swift
+//  reactionTime
+//
+//  Created by Kevin O'Connell on 12/5/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+import UIKit
+
+class timer: Timer {
+var seconds = Timer()
+    
+var oneSecond = 1
+var  twoSeconds = 2
+var threeSeconds = 3
+var fourSeconds = 4
+var fiveSeconds = 5
+var sixSeconds = 6
+var sevenSeconds = 7
+var eightSeconds = 8
+var nineSeconds = 9
+var tenSeconds = 10
+    
+}
